6324e6d831f5c143319156e1d4a2be73d1fcaf26
Isaac Vitor <isaacvitor@gmail.com>
2023-03-14 23:30:35 +0000

    Added AsyncQueueObserver

--- src/pipeline/observers.py
+++ src/pipeline/observers.py
@@ -3,7 +3,7 @@ import asyncio
 import multiprocessing as mp
 
 from abc import ABC, abstractmethod
-from typing import Union, Optional
+from typing import Union, Optional, Callable, Any, Coroutine
 
 
 class QueueProtocol(ABC):
@@ -11,7 +11,20 @@ class QueueProtocol(ABC):
     @abstractmethod
     def __init__(
             self, input_queue: Union[asyncio.Queue, mp.Queue],
+            on_item: Callable[..., Coroutine[Any, Any, Any]],
             output_queue: Optional[Union[asyncio.Queue, mp.Queue]]) -> None:
+        ...
+
+    @abstractmethod
+    def start(self) -> asyncio.Future:
+        ...
+
+    @abstractmethod
+    def stop(self) -> None:
+        ...
+
+    @abstractmethod
+    async def _observer(self) -> None:
         ...
 
     @abstractmethod
@@ -21,11 +34,35 @@ class QueueProtocol(ABC):
 
 class AsyncQueueObserver(QueueProtocol):
 
-    def __init__(self,
-                 input_queue: asyncio.Queue,
-                 output_queue: Optional[asyncio.Queue] = None) -> None:
-        self.__input_queue = input_queue
-        self.__output_queue = output_queue
+    def __init__(
+        self,
+        input_queue: asyncio.Queue,
+        on_item: Callable[..., Coroutine[Any, Any, Any]],
+        output_queue: Optional[asyncio.Queue] = None,
+    ) -> None:
+        self.__input_queue: asyncio.Queue = input_queue
+        self.__output_queue: Union[asyncio.Queue, None] = output_queue
+        self.observer_task: asyncio.Future
+        self.on_item: Callable[..., Coroutine[Any, Any, Any]] = on_item
+
+    def start(self) -> asyncio.Future:
+        self.observer_task = asyncio.create_task(self._observer())
+        return self.observer_task
+
+    def stop(self) -> None:
+        self.observer_task.cancel()
+
+    async def _observer(self) -> None:
+        while True:
+            if self.__input_queue.qsize():
+                task: Any = await self.__input_queue.get()
+                result: Any = await self.on_item(task)
+                self.__input_queue.task_done()
+
+                if self.__output_queue is not None:
+                    self.__output_queue.put_nowait(result)
+            else:
+                await asyncio.sleep(0)
 
     def __len__(self) -> int:
         return self.__input_queue.qsize()
--- tests/unit/test_asyncio_queue_observer.py
+++ tests/unit/test_asyncio_queue_observer.py
@@ -1,4 +1,5 @@
 import asyncio
+from typing import Any
 from unittest.mock import Mock
 
 import pytest
@@ -16,9 +17,13 @@ def output_asyncio_queue() -> asyncio.Queue:
     return asyncio.Queue()
 
 
+async def nope(element: Any) -> Any:
+    return element
+
+
 def test_no_elements_in_the_input_queue(input_queue: asyncio.Queue) -> None:
 
-    async_queue_observer = AsyncQueueObserver(input_queue)
+    async_queue_observer = AsyncQueueObserver(input_queue, on_item=nope)
 
     assert len(async_queue_observer) == 0
 
@@ -26,7 +31,8 @@ def test_no_elements_in_the_input_queue(input_queue: asyncio.Queue) -> None:
 def test_one_element_in_the_input_queue(input_queue: asyncio.Queue) -> None:
     element: Mock = Mock()
     input_queue.put_nowait(element)
-    async_queue_observer = AsyncQueueObserver(input_queue)
+    async_queue_observer: AsyncQueueObserver = AsyncQueueObserver(input_queue,
+                                                                  on_item=nope)
 
     assert len(async_queue_observer) == 1
 
@@ -38,9 +44,56 @@ def test_get_element_input_queue(input_queue: asyncio.Queue) -> None:
     input_queue.put_nowait(element_one)
     input_queue.put_nowait(element_two)
 
-    async_queue_observer = AsyncQueueObserver(input_queue)
+    async_queue_observer: AsyncQueueObserver = AsyncQueueObserver(input_queue,
+                                                                  on_item=nope)
 
     retrieved_element: Mock = input_queue.get_nowait()
 
     assert len(async_queue_observer) == 1
     assert retrieved_element == element_one
+
+
+@pytest.mark.asyncio
+async def test_start_observer_must_return_a_future(
+        input_queue: asyncio.Queue) -> None:
+    async_queue_observer: AsyncQueueObserver = AsyncQueueObserver(input_queue,
+                                                                  on_item=nope)
+
+    observer_task: asyncio.Future = async_queue_observer.start()
+
+    assert isinstance(observer_task, asyncio.Future)
+
+
+@pytest.mark.asyncio
+async def test_stop_observer_cancel_observer_task(
+        input_queue: asyncio.Queue) -> None:
+    async_queue_observer: AsyncQueueObserver = AsyncQueueObserver(input_queue,
+                                                                  on_item=nope)
+
+    observer_task: asyncio.Future = async_queue_observer.start()
+
+    async_queue_observer.stop()
+
+    with pytest.raises(asyncio.CancelledError):
+        await observer_task
+
+
+@pytest.mark.asyncio
+async def test_output_queue_get_value(input_queue: asyncio.Queue,
+                                      output_queue: asyncio.Queue) -> None:
+
+    async def increase_a_number(number: int) -> int:
+        number = number + 1
+        return number
+
+    async_queue_observer: AsyncQueueObserver = AsyncQueueObserver(
+        input_queue, on_item=increase_a_number, output_queue=output_queue)
+
+    async_queue_observer.start()
+    input_queue.put_nowait(1)
+
+    await asyncio.sleep(0)
+
+    assert output_queue.qsize() != 0
+    result: Any = output_queue.get_nowait()
+    assert result == 2
